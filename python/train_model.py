f720aff5049a57adcc879a7dfdad105be6f631e0
KumarShresth7 <kumarshresth2004@gmail.com>
2024-10-26 15:41:27 +0530

    model completed

--- python/train_model.py
+++ python/train_model.py
@@ -59,11 +59,25 @@ new_df['tags'] = new_df['tags'].apply(lambda x:x.lower())
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=5000,stop_words='english')
 vector = cv.fit_transform(new_df['tags']).toarray()
-print(vector)
+# print(vector)
 
 from sklearn.metrics.pairwise import cosine_similarity
 similarity = cosine_similarity(vector)
 
+def recommend(movie):
+    movie_index = new_df[new_df['title']==movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x: x[1])[1:6]
+    
+    for i in movies_list:
+        print(new_df.iloc[i[0]].title)
+
+print(recommend('Avatar'))
+
+#create pickle model
+
+    
+        
 
 
 
